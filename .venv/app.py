25959f01c3fb6a42cd55ddb31fe7ceefc13cb521
E. Junior Rodriguez <Jrah892@gmail.com>
2026-01-07 07:34:38 -0500

    Render deployment config

--- .venv/app.py
+++ .venv/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -197,6 +198,6 @@ def override_truck():
 
     return redirect(url_for("index"))
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
